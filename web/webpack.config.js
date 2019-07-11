a219a9de25fd5cea86f113557564b774120116f5
Sean Joyal <sean.joyal@gmail.com>
2019-07-11 12:12:36 -0400

    fix eslint vs prettier argument. prettier wins

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,31 +1,28 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    entry: {
-        main: path.resolve(__dirname, 'src/main.js'),
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: {
+    main: path.resolve(__dirname, 'src/main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    alias: {
+      '@shared': path.resolve(__dirname, 'src/shared'),
+      '@store': path.resolve(__dirname, 'src/store'),
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-    },
-    resolve: {
-        alias: {
-            '@shared': path.resolve(__dirname, 'src/shared'),
-            '@store': path.resolve(__dirname, 'src/store'),
-        },
-        modules: ['node_modules'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader',
-                    'eslint-loader',
-                ],
-            },
-        ]
-    }
+    modules: ['node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
 }
