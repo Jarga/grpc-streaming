2d5a7b513aa27060bba5508b79af3b7eaae64a8d
Sean McAdams <sean.j.mcadams@gmail.com>
2019-07-21 12:34:05 -0400

    Add sql, update comment server

--- grpc-chat-server/Startup.cs
+++ grpc-chat-server/Startup.cs
@@ -2,20 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using grpc_chat_server.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace grpc_chat_server
 {
     public class Startup
     {
+        public Startup(IWebHostEnvironment env, IConfiguration configuration)
+        {
+            Configuration = configuration;
+            HostingEnvironment = env;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment HostingEnvironment { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+
+            services.AddLogging(config => config.AddConsole());
+            services.AddTransient<ChatRepository>();
+
+            services.AddSingleton(Configuration);
+            services.AddSingleton((services) =>
+            {
+                var config = Configuration.GetConnectionString("Redis");
+                return ConnectionMultiplexer.Connect("localhost");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
