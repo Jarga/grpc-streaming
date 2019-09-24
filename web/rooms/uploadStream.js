2fe51659bceaabb241ca899fcfd267eb28c07786
King <tking@advent.com>
2019-09-24 11:27:39 -0400

    Removed Pesky ;

--- web/rooms/uploadStream.js
+++ web/rooms/uploadStream.js
@@ -3,13 +3,13 @@ const logger = require('../logging').createLogger('uploadStream')
 let activeStreams = {}
 
 module.exports.uploadStream = function (io, socket, videoService) {
-    var user_id = socket.handshake.query.user_id;
+    var user_id = socket.handshake.query.user_id
 
-    logger.info(`User ${user_id} started video`);
+    logger.info(`User ${user_id} started video`)
 
     //TODO: Manage activeStreams better, this can cause issues with multiple open streams
     activeStreams[user_id] = videoService.uploadStream(function(error, data) {
-        console.log('Func call', error, JSON.stringify(data));
+        console.log('Func call', error, JSON.stringify(data))
     })
     socket.on('video_chunk', function (data) {
         //TODO: Allow setting name/id/etc
@@ -17,6 +17,6 @@ module.exports.uploadStream = function (io, socket, videoService) {
     })
     socket.on('disconnect', function () {
         activeStreams[user_id].end()
-        logger.info(`User ${user_id} disconnected video`);
+        logger.info(`User ${user_id} disconnected video`)
     });
 };
